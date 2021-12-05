0b4532aa4982c520997c6cf9982c056dbc493521
sudipta9 <sudiptapradhan58271@gmail.com>
2021-12-05 22:20:09 +0530

    __

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import SignUp from "./components/SignUp";
 import {} from "react-router-dom";
-import { Route, Routes } from "react-router";
+import { Route, Routes, useNavigate } from "react-router";
 import { createGlobalStyle } from "styled-components";
 import SignIn from "./components/SignIn";
 import Home from "./components/Home";
+import Cookies from "universal-cookie";
 
 const GlobalStyle = createGlobalStyle`
   
 `;
 const App = () => {
+  const cookie = useMemo(() => {
+    return new Cookies();
+  }, []);
+
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = cookie.get("auth-token");
+    if (token) {
+      // setIsLoggedIn(true);
+      navigate("/home");
+    } else {
+      navigate("/sign-in");
+    }
+  }, [cookie, navigate]);
   return (
     <>
       <GlobalStyle />
@@ -18,11 +35,12 @@ const App = () => {
           <Route
             path="/*"
             exact={true}
-            element={<p className="text-center">404 not found</p>}
+            element={<p className="text-center h4">404 not found</p>}
           ></Route>
           <Route path="/sign-up" exact={true} element={<SignUp />}></Route>
           <Route path="/sign-in" exact={true} element={<SignIn />}></Route>
           <Route path="/home" exact={true} element={<Home />}></Route>
+          <Route path="/" exact />
         </Routes>
       </div>
     </>
