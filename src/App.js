f4d9cb1f79dca7d7a7f166e80f244b595f4b11eb
sudipta9 <sudiptapradhan58271@gmail.com>
2022-01-24 22:01:54 +0530

    __

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React from "react";
 import SignUp from "./components/SignUp";
 import {} from "react-router-dom";
-import { Route, Routes, useNavigate } from "react-router";
+import { Route, Routes } from "react-router";
 import { createGlobalStyle } from "styled-components";
 import SignIn from "./components/SignIn";
 import Home from "./components/Home";
-import Cookies from "universal-cookie";
+// import Cookies from "universal-cookie";
 
 const GlobalStyle = createGlobalStyle`
   
 `;
 const App = () => {
-  const cookie = useMemo(() => {
-    return new Cookies();
-  }, []);
+  // const cookie = useMemo(() => {
+  //   return new Cookies();
+  // }, []);
 
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    const token = cookie.get("auth-token");
-    if (token) {
-      // setIsLoggedIn(true);
-      navigate("/home");
-    } else {
-      navigate("/sign-in");
-    }
-  }, [cookie, navigate]);
+  // useEffect(() => {
+  //   const token = cookie.get("auth-token");
+  //   if (token) {
+  //     // setIsLoggedIn(true);
+  //     navigate("/home");
+  //   } else {
+  //     navigate("/sign-in");
+  //   }
+  // }, [cookie, navigate]);
   return (
     <>
       <GlobalStyle />
@@ -40,7 +40,9 @@ const App = () => {
           <Route path="/sign-up" exact={true} element={<SignUp />}></Route>
           <Route path="/sign-in" exact={true} element={<SignIn />}></Route>
           <Route path="/home" exact={true} element={<Home />}></Route>
-          <Route path="/" exact />
+          {/* <Route path="/" exact>
+            {navigate("/sign-in")}
+          </Route> */}
         </Routes>
       </div>
     </>
